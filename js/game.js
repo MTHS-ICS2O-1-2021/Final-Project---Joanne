fa2b4dee534be4bd12d141c2c4a626a3bdf07819
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-06 17:23:56 +0000

    create game scene

---
+++ js/game.js
@@ -0,0 +1,20 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: June 2022
+// This is the Cookie Monster configuration file
+
+/**
+ * Start Cookie Monster Game
+ */
+const config = {
+  type: Phaser.AUTO, 
+  width: 1920,
+  height: 1080,
+  // set background color
+  backgroundColor: 0x5f6e7a,
+}
+
+const game = new Phaser.Game(config)
