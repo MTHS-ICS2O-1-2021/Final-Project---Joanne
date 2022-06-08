4ea856feee6ecb1aefcb69d185d6847b22bd5882
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-08 21:58:26 +0000

    add title scene

--- js/menuScene.js
+++ js/menuScene.js
@@ -1,30 +1,20 @@
 /* global Phaser */
 
-/* global Phaser */
-
 // Copyright (c) 2022 Joanne Santhosh All rights reserved
 //
 // Created by: Joanne Santhosh
 // Created on: June 2022
-// This is the Title Scene
+// This is the Menu Scene
 
 /**
- * This class is the Title Scene.
+ * This class is the Menu Scene.
  */
-class TitleScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene {
   /**
    * This method is the construtor.
    */
   constructor() {
-    super({ key: "titleScene" })
-
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
-    this.titleSceneTextStyle = {
-      font: "200px Times",
-      fill: "#fde4b9",
-      align: "center",
-    }
+    super({ key: "menuScene" })
   }
 
   /**
@@ -42,8 +32,7 @@ class TitleScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log("Title Scene")
-    this.load.image("titleSceneBackground", "assets/TA1sFh.jpeg")
+    console.log("Menu Scene")
   }
 
   /**
@@ -52,11 +41,7 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.titleSceneBackgroundImage = this.add.sprite(0, 0, "titleSceneBackground").setScale(2.75)
-    this.titleSceneBackgroundImage.x = 1000 / 2
-    this.titleSceneBackgroundImage.y = 900 / 2
-
-     this.titleSceneText = this.add.text(1920 / 2, 1080 / 2 + 350, "Cookie Monster", this.titleSceneTextStyle).setOrigin(0.5)
+    // pass
   }
 
   /**
@@ -70,4 +55,4 @@ class TitleScene extends Phaser.Scene {
   }
 }
 
-export default TitleScene
+export default MenuScene
