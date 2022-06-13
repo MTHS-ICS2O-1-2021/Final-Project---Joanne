3686d3b524d4b660546bbecc1d33931cde398663
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-13 17:25:29 +0000

    update code

--- js/gameScene.js
+++ js/gameScene.js
@@ -61,7 +61,50 @@ class GameScene extends Phaser.Scene {
    *  @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
-    // pass
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keyUpObj = this.input.keyboard.addKey("UP")
+    const keyDownObj = this.input.keyboard.addKey("DOWN")
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+       if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+
+    if (keyUpObj.isDown === true) {
+      if (this.eatCookie === false) {
+        // eat cookie
+        this.eatCookie = true
+        const aNewCookie = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "cookie"
+        )
+        this.cookieGroup.add(aNewCookie)
+      }
+    }
+
+    if (keyLeftObj.isUp === true) {
+      this.eatCookie = false
+    }
+    if (keyRightObj.isUp === true) {
+      this.eatCookie = false
+    }
+    if (keyUpObj.isUp === true) {
+      this.eatCookie = false
+    }
+    if (keyDownObj.isUp === true) {
+      this.eatCookie = false
+    }
   }
 }
 
