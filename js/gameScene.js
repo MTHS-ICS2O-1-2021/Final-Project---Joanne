28083a2d69b6bceb67ca14132a27beb81fe8d456
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-15 17:26:38 +0000

    update code

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,18 +4,19 @@
 //
 // Created by: Joanne Santhosh
 // Created on: June 2022
-// This is the Menu Scene
+// This is the Game Scene
 
 /**
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
+  
   // create a cookie
   createACookie() {
     const cookieXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
     let cookieXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
     cookieXVelocity *= Math.round(Math.random()) ? 1 : -1 // thiis will add minus sign in 50% of cases
-    const aCookie = this.physics.add.sprite(cookieXVelocity, -100, "cookie")
+    const aCookie = this.physics.add.sprite(cookieXLocation, -100, "cookie")
     aCookie.body.velocity.y = 200
     aCookie.body.velocity.x = cookieXVelocity
     this.cookieGroup.add(aCookie)
@@ -31,20 +32,10 @@ class GameScene extends Phaser.Scene {
     this.cookieMonster = null
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * This method is called by the Scene Manager when the scene starts,
-   *   before preload() and create().
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to load assets.
-   */
   preload() {
     console.log("Game Scene")
 
@@ -57,14 +48,9 @@ class GameScene extends Phaser.Scene {
     )
 
     // sound
-    this.load.audio("crunch", "assets/crack-and-crunch-14891.mp3")
+    this.load.audio('crunch', "assets/crack-and-crunch-14891.mp3")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
     this.background = this.add.image(0, 0, "gameBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
@@ -74,26 +60,21 @@ class GameScene extends Phaser.Scene {
       1080 - 100,
       "cookieMonster"
     )
+    
     // create cookies
     this.cookieGroup = this.add.group()
     this.createACookie()
 
     // Collissions between cookie monster and cookies
     this.physics.add.collider(this.cookieMonster, this.cookieGroup, function (cookieCollide, cookieMonsterCollide){
-      cookieCollide.destroy()
       cookieMonsterCollide.destroy()
-      this.sound.play("crunch")
+      this.sound.play('crunch')
       this.createACookie()
       this.createACookie()
     }.bind(this))
   }
+}
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   *  @param {number} time - The current time.
-   *  @param {number} delta - The delta time in ms since the last frame.
-   */
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
@@ -127,8 +108,7 @@ class GameScene extends Phaser.Scene {
         this.cookieMonster.y = 1920
       }
     }
-    if (keySpaceObj.isUp === true) {
-       this.createACookie = false
-    }
+  }
+}
     
-  export default GameScene
+export default GameScene
